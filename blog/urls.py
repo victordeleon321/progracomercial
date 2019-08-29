18f9fc6777a8a634859083d1f8087456406b842a
victordeleon321 <victordeleonesc321@gmail.com>
2019-08-28 18:26:19 -0600

    Se agrego una plantilla HTML Se hizo un queryset sobre las publicaciones ya publicadas

---
+++ blog/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.listar_pub, name='listar_pub'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
 
+from .models import Publicacion
 # Create your views here.
+
+def listar_pub(request):
+    pubs = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/listar_pub.html', {'pubs': pubs})
