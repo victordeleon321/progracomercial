9be143c7e2b74636a64df0be96d085f01e756077
victordeleon321 <victordeleonesc321@gmail.com>
2019-09-04 18:26:40 -0600

    Se agrego css y boostrap Vistas y plantilla para el detalle del post

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.listar_pub, name='listar_pub'),
+    path('pub/<int:pk>/', views.detalle_pub, name='detalle_pub')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Publicacion
@@ -7,3 +7,7 @@ from .models import Publicacion
 def listar_pub(request):
     pubs = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/listar_pub.html', {'pubs': pubs})
+
+def detalle_pub(request,pk):
+    p = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/detalle_pub.html', {'p': p})
